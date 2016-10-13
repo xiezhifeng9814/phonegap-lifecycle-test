b36b11080d59a2b99f855f7797a68b37ae1b3647
xiezhifeng9814 <xiez1_16@uni.worc.ac.uk>
2016-10-13 14:46:24 +0100

    3

--- www/js/index.js
+++ www/js/index.js
@@ -10,7 +10,7 @@ var launched_count = 0;
 	function updateDisplay() {
 		$("#launched").text("Application launched: " + launched_count);
 //		$("#resumed").text("Application paused: " + paused_count);
-        document.getElementById('resume').text="Application paused: " + paused_count;
+        document.getElementById('resumed').text="Application paused: " + paused_count;
 		$("#paused").text("Application resumed: " + resumed_count);
 	}
 
